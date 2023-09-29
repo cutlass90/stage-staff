01195a403eabaacc0c1a7ae6c4bb1bb802118e95
Nazar Shmatko <ns@reface.ai>
2023-09-29 11:57:59 -0400

    stage generation streamlit demo works properly

---
+++ main.py
@@ -0,0 +1,81 @@
+import os
+
+import torch
+import numpy as np
+from PIL import Image
+
+from transformers import DPTFeatureExtractor, DPTForDepthEstimation
+from diffusers import ControlNetModel, StableDiffusionXLControlNetPipeline, AutoencoderKL
+
+class StageGenerator:
+
+    def __init__(self, device):
+        self.device = device
+        self.depth_estimator = DPTForDepthEstimation.from_pretrained("Intel/dpt-hybrid-midas").to(self.device)
+        self.feature_extractor = DPTFeatureExtractor.from_pretrained("Intel/dpt-hybrid-midas")
+        controlnet = ControlNetModel.from_pretrained(
+            "diffusers/controlnet-depth-sdxl-1.0",
+            variant="fp16",
+            use_safetensors=True,
+            torch_dtype=torch.float16,
+        ).to(self.device)
+        vae = AutoencoderKL.from_pretrained("madebyollin/sdxl-vae-fp16-fix", torch_dtype=torch.float16).to(self.device)
+        self.pipe = StableDiffusionXLControlNetPipeline.from_pretrained(
+            "stabilityai/stable-diffusion-xl-base-1.0",
+            controlnet=controlnet,
+            vae=vae,
+            variant="fp16",
+            use_safetensors=True,
+            torch_dtype=torch.float16,
+        ).to(self.device)
+        self.pipe.enable_model_cpu_offload()
+
+    def get_depth_map(self, image:Image.Image) -> Image.Image:
+        image = image.convert("RGB")
+        image = self.feature_extractor(images=image, return_tensors="pt").pixel_values.to(self.device)
+        with torch.no_grad(), torch.autocast('cuda'):
+            depth_map = self.depth_estimator(image).predicted_depth
+
+        depth_map = torch.nn.functional.interpolate(
+            depth_map.unsqueeze(1),
+            size=(1024, 1024),
+            mode="bicubic",
+            align_corners=False,
+        )
+        depth_min = torch.amin(depth_map, dim=[1, 2, 3], keepdim=True)
+        depth_max = torch.amax(depth_map, dim=[1, 2, 3], keepdim=True)
+        depth_map = (depth_map - depth_min) / (depth_max - depth_min)
+        image = torch.cat([depth_map] * 3, dim=1)
+
+        image = image.permute(0, 2, 3, 1).cpu().numpy()[0]
+        image = Image.fromarray((image * 255.0).clip(0, 255).astype(np.uint8))
+        return image
+
+
+    def __call__(self, prompt, depth_image: Image.Image, steps=30, controlnet_conditioning_scale = 0.5) -> Image.Image:
+        result = self.pipe(
+            prompt, image=depth_image, num_inference_steps=30,
+            controlnet_conditioning_scale=controlnet_conditioning_scale,
+        ).images[0]
+        return result
+
+def main(prompt, input_image_path, device):
+    stage_generator = StageGenerator(device)
+    depth_map = stage_generator.get_depth_map(Image.open(input_image_path))
+    depth_map.save('data/depth_map.png')
+    generated = stage_generator(prompt, depth_map)
+    generated.save('data/generated.png')
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    # prompt = "Sidescroller game level of an 80's cyberpunk style scrap yard, arcade sidescroller, pixel art, arcade, low contrast, flat art, shadowrun, bladerunner, vladimir trechikoff, stylized comic, game art, hand painted, low detail"
+    prompt = "a city street filled with lots of tall buildings, muted deep neon color, metal slug concept art, chasm, 8 k highly detailed ❤🔥 🔥 💀 🤖 🚀, lofi artstyle, background ( dark _ smokiness ), hand - drawn 2 d art, industrial space, 21:9, garage, by senior artist, unknown space, dark blurry background"
+    main(prompt, 'data/3_1.jpg', 'cuda:0')
---
+++ streamlit_demo.py
@@ -0,0 +1,44 @@
+import streamlit as st
+from main import StageGenerator
+from PIL import  Image
+
+st.set_page_config(layout="wide")
+st.title("Stage Generator Demo")
+
+
+PROMPT = "a city street filled with lots of tall buildings, muted deep neon color, metal slug concept art, chasm, 8 k highly detailed ❤🔥 🔥 💀 🤖 🚀, lofi artstyle, background ( dark _ smokiness ), hand - drawn 2 d art, industrial space, 21:9, garage, by senior artist, unknown space, dark blurry background"
+DEVICE = "cuda:1"
+
+
+def main():
+    if 'stage_generator' not in st.session_state:
+        with st.spinner(text="Loading models. Please wait..."):
+            st.session_state['stage_generator'] = StageGenerator(device=DEVICE)
+    prompt = st.text_input("Prompt", PROMPT)
+    controlnet_conditioning_scale = st.number_input('controlnet_conditioning_scale', min_value=0.0, max_value=1.0, value=0.5, step=0.01)
+    steps = st.number_input('steps', min_value=1, max_value=100,     value=30, step=1)
+
+    input_image = st.file_uploader("Choose an image", type=["jpg", "jpeg", "png"])
+    if input_image is not None:
+        col1, col2, col3 = st.columns(3)
+        image = Image.open(input_image)
+        with col1:
+            st.image(image, use_column_width=True)
+        if st.button("RUN"):
+            with st.spinner(text="Working. Please wait..."):
+                depth_image = st.session_state['stage_generator'].get_depth_map(image)
+                with col2:
+                    st.image(depth_image, use_column_width=True)
+                result = st.session_state['stage_generator'](prompt, depth_image, steps, controlnet_conditioning_scale)
+                with col3:
+                    st.image(result, use_column_width=True)
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
